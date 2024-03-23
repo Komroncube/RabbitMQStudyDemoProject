ff63e8fc5192225e752ed6b017dfa73833132b6d
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-03-23 17:21:29 +0500

    complete createTransactionConsumer class

--- RabbitMQStudyDemoProject/Consumers/CreateTransactionConsumer.cs
+++ RabbitMQStudyDemoProject/Consumers/CreateTransactionConsumer.cs
@@ -1,5 +1,20 @@
-﻿namespace RabbitMQStudyDemoProject.Consumers;
+﻿using MassTransit;
+using RabbitMQStudyDemoProject.Models;
 
-public class CreateTransactionConsumer
+namespace RabbitMQStudyDemoProject.Consumers;
+
+public class CreateTransactionConsumer : IConsumer<CreateTransaction>
 {
+    private readonly ILogger<CreateTransactionConsumer> _logger;
+
+    public CreateTransactionConsumer(ILogger<CreateTransactionConsumer> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Consume(ConsumeContext<CreateTransaction> context)
+    {
+        _logger.LogInformation($"Consume data from createTransactionQueue {context.Message.CardNumber}");
+        return Task.CompletedTask;
+    }
 }
