7b895e846116fd49e2a6731367b0d0e064ca9d69
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-03-23 17:22:37 +0500

    Add createTransaction and notifyTransaction methods

--- RabbitMQStudyDemoProject/Controllers/MessageProducerController.cs
+++ RabbitMQStudyDemoProject/Controllers/MessageProducerController.cs
@@ -1,14 +1,22 @@
 ﻿using System.Text;
 using System.Text.Json;
+using MassTransit;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RabbitMQ.Client;
+using RabbitMQStudyDemoProject.Models;
 
 namespace RabbitMQStudyDemoProject.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class MessageProducerController : ControllerBase
 {
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public MessageProducerController(IPublishEndpoint publishEndpoint)
+    {
+        _publishEndpoint = publishEndpoint;
+    }
 
     [HttpPost("send")]
     public IActionResult Send(string message)
@@ -30,6 +38,19 @@ public class MessageProducerController : ControllerBase
                 body:body);
 
         }   
+        return Ok();
+    }
+
+    [HttpPost("transactions/notify")]
+    public IActionResult SendNotify(NotifyTransaction transaction)
+    {
+        _publishEndpoint.Publish(transaction);
+        return Ok();
+    }
+    [HttpPost("transactions/create")]
+    public IActionResult SendTransaction(CreateTransaction transaction)
+    {
+        _publishEndpoint.Publish(transaction);
         return Ok();
     }
 }
